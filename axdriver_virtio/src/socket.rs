90b1ac7b7a4425eacdcd1e9ce010a6ab0b47f043
huangshijia <huangshijia@kylinos.cn>
2025-12-29 14:47:57 +0800

    maybe never request?

--- axdriver_virtio/src/socket.rs
+++ axdriver_virtio/src/socket.rs
@@ -77,7 +77,7 @@ impl<H: Hal, T: Transport> VsockDriverOps for VirtIoSocketDev<H, T> {
         let res = self.inner
             .recv(peer_addr, src_port, buf)
             .map_err(as_dev_err);
-        self.inner.update_credit(peer_addr, src_port);
+        // self.inner.update_credit(peer_addr, src_port);
         res
     }
 
